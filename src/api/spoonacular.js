aff0328fe4d1ababb05dfcbf05cb617961b9c5a3
Zed <zedcaoile@gmail.com>
2025-08-25 18:31:17 +0800

    modularize api calls into separate file

---
+++ src/api/spoonacular.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = "https://api.spoonacular.com";
+
+const fetchIngredientSuggestions = async (query) => {
+  const response = await axios.get(
+    `${BASE_URL}/food/ingredients/autocomplete`,
+    {
+      params: {
+        query,
+        metaInformation: true,
+        number: 5,
+        apiKey: API_KEY,
+      },
+    }
+  );
+  return response.data;
+};
+
+const fetchRecipesByIngredients = async (ingredientNames) => {
+  const response = await axios.get(`${BASE_URL}/recipes/findByIngredients`, {
+    params: {
+      ingredients: ingredientNames,
+      number: 10,
+      ranking: 1,
+      apiKey: API_KEY,
+    },
+  });
+  return response;
+};
+
+export { fetchIngredientSuggestions, fetchRecipesByIngredients };
