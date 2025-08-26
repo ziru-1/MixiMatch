1ee55116c3bb90c9e35945bc9be7e76c1a47217c
Zed <zedcaoile@gmail.com>
2025-08-26 18:46:02 +0800

    refactor RecipeList by extracting inner recipe into its own component

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React from "react";
 
-const RecipeList = ({recipes, onRecipeClick}) => {
+const Recipe = ({ recipe, onRecipeClick }) => {
+  return (
+    <div key={recipe.id} onClick={() => onRecipeClick(recipe.id)}>
+      <h3>{recipe.title}</h3>
+      <img src={recipe.image} alt={recipe.title} />
+      <p>Used Ingredients: {recipe.usedIngredientCount}</p>
+      <p>Missed Ingredients: {recipe.missedIngredientCount}</p>
+    </div>
+  );
+};
+
+const RecipeList = ({ recipes, onRecipeClick }) => {
   return (
     <div>
-        <h2>Recipes</h2>
-        {recipes.map((recipe) => {
-          return (
-            <div key={recipe.id} onClick={() => onRecipeClick(recipe.id)}>
-              <h3>{recipe.title}</h3>
-              <img src={recipe.image} alt={recipe.title} />
-              <p>Used Ingredients: {recipe.usedIngredientCount}</p>
-              <p>Missed Ingredients: {recipe.missedIngredientCount}</p>
-            </div>
-          );
-        })}
-      </div>
-  )
-}
+      <h2>Recipes</h2>
+      {recipes.map((recipe) => {
+        return (
+          <Recipe
+            key={recipe.id}
+            recipe={recipe}
+            onRecipeClick={onRecipeClick}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
-export default RecipeList
+export default RecipeList;
